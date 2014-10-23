1124907aa4ae6d5c33dd141997d3d08715c3ab3e
Bruno Sesso <bsesso@ime.usp.br>
2014-10-23 17:01:16 -0200

    To testando os testes... hahaah que ironico

--- Tests/test_Move.py
+++ Tests/test_Move.py
@@ -1,21 +1,21 @@
 import unittest
 import sys, os
 sys.path.insert(0, os.path.abspath('../src/'));
-from Attack import Attack
+from Move import Move
 from Type import Type
 
-class TestAttack(unittest.TestCase):
+class TestMove(unittest.TestCase):
 	def setUp(self):
 		self.right_name="Tackle"
-		self.right_elm_type = Type.normal
+		self.right_elm_type = Type.Normal
 		self.right_accuracy = 100 #Sera que isso não é um float de 0 ~ 1??
 		self.right_power = 50
 		self.right_pp = 35
 
 	def test_stab(self):
-		fire_type = Type.fire
-		water_type = Type.water
-		atk = Attack(elm_type=Type.fire, name=self.right_name, \
+		fire_type = Type.Fire
+		water_type = Type.Water
+		atk = Move(self.right_name, Type.Fire, \
 				self.right_accuracy, self.right_power, self.right_pp)
 
 		# Test results
@@ -28,33 +28,33 @@ class TestAttack(unittest.TestCase):
 
 	def test_init(self):
 		# Test wrong parameters passing
-		assertRaises(ValueError, Attack(self, self.right_elm_type, \
+		self.assertRaises(ValueError, Move.__init__(self, self, self.right_elm_type, \
 				self.right_accuracy, self.right_power, self.right_pp))
-		assertRaises(ValueError, Attack(self.right_name, "forninho", \
+		self.assertRaises(ValueError, Move.__init__(self, self.right_name, "forninho", \
 				self.right_accuracy, self.right_power, self.right_pp))
-		assertRaises(ValueError, Attack(self.right_name, self.right_elm_type, \
+		self.assertRaises(ValueError, Move.__init__(self, self.right_name, self.right_elm_type, \
 				"something", self.right_power, self.right_pp))
-		assertRaises(ValueError, Attack(self.right_name, self.right_elm_type, \
+		self.assertRaises(ValueError, Move.__init__(self, self.right_name, self.right_elm_type, \
 				self.right_accuracy, "Catorze", self.right_pp))
-		assertRaises(ValueError, Attack(self.right_name, self.right_elm_type, \
+		self.assertRaises(ValueError, Move.__init__(self, self.right_name, self.right_elm_type, \
 				self.right_accuracy, self.right_power, "Catorze"))
 		
 		# Test right parameters passing
-		assertRaises(None, self.right_name, self.right_elm_type, \
+		self.assertRaises(None, Move.__init__(self, self.right_name, self.right_elm_type, \
 				self.right_accuracy, self.right_power, self.right_pp))
-		assertRaises(None, Attack(Name=1234, elm_type=self.right_elm_type, \
-				"7", "7", "7"))
+		self.assertRaises(None, Move.__init__(self, name=1234, elm_type=self.right_elm_type, \
+				accuracy="7", power="7", pp="7"))
 
 		# Test absurds!
-		assertRaises(ValueError, Attack(self.right_name, Type.blank, \
+		self.assertRaises(ValueError, Move.__init__(self, self.right_name, Type.Blank, \
 				self.right_accuracy, self.right_power, self.right_pp))
-		assertRaises(ValueError, Attack(self.right_name, self.right_type, \
+		self.assertRaises(ValueError, Move.__init__(self, self.right_name, self.right_type, \
 				101, self.right_power, self.right_pp))
-		assertRaises(ValueError, Attack(self.right_name, self.right_type, \
+		self.assertRaises(ValueError, Move.__init__(self, self.right_name, self.right_type, \
 				-1, self.right_power, self.right_pp))
-		assertRaises(ValueError, Attack(self.right_name, self.right_type, \
+		self.assertRaises(ValueError, Move.__init__(self, self.right_name, self.right_type, \
 				self.right_accuracy, -1, self.right_pp))
-		assertRaises(ValueError, Attack(self.right_name, self.right_type, \
+		self.assertRaises(ValueError, Move.__init__(self, self.right_name, self.right_type, \
 				self.right_accuracy, self.right_power, -1))
 
 if __name__ == '__main__':
