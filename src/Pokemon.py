04faf9c92c006c2f2bb25c57dc00dbc0eb0e1e51
Bruno Sesso <bruno.sesso@usp.br>
2014-10-23 20:54:15 -0200

    Consertei a Pokemon

--- src/Pokemon.py
+++ src/Pokemon.py
@@ -1,92 +1,105 @@
 class Pokemon:
-	'''
-	Class that defines the properties, attack and defense of the Pokemon
-	'''
-	def __init__(self, poke_type1, poke_type2 = 'none', stats, name = 'unknown', level = 1, move_list = []):
-		''' Description TODO
+    '''
+    Class that defines the properties, attack and defense of the Pokemon
+    '''
+    def __init__(self, poke_type1, poke_type2 = 'none', stats, name = 'unknown', level = 1, move_list = []):
+        ''' Description TODO
 
-		:param poke_type1: Pokemon's principal type
-		:param poke_type2: Pokemon's secondary type (default: none)
-		:param stats: Pokemon's stats, object of the Stats class.
-		:param name: Pokemon's name (for example, a Zubat's name is 'Batman')
-		:param level: Pokemon's level
-		:param move_list: Pokemon's moves, object of the Move class.
-		:returns: TODO
-		'''
-		self._name = name
-		self._level = level
-		self._poke_type = poke_type
-		self._stats = stats
-		self._attack_list = attack_list
+        :param poke_type1: Pokemon's principal type
+        :param poke_type2: Pokemon's secondary type (default: none)
+        :param stats: Pokemon's stats, object of the Stats class.
+        :param name: Pokemon's name (for example, a Zubat's name is 'Batman')
+        :param level: Pokemon's level
+        :param move_list: Pokemon's moves, object of the Move class.
+        :returns: TODO
+        '''
+        self._name = name
+        self._level = level
+        self._poke_type = poke_type
+        self._stats = stats
+        self._attack_list = attack_list
 
-	@property
-	def name(self):
-		'''Get pokemon's name'''
-		return self._name
-	@name.setter
-	def name(self, value):
-		'''Set pokemon's name'''
-		self._name = value
-	
-	@property
-	def level(self):
-		'''Get pokemon's level'''
-		return self._level
-	@level.setter
-	def level(self, value):
-		'''Set pokemon's level'''
-#### TESTAR PARA AS OUTRAS PROPERTIES ASSIM COMO ESSE TRY: ######	
-		try:
-			value = int(value)
-		except ValueError:
-			print ("ERROR: Tried to assign a not int value to pokemon's level")
-		else:
-			self._level = value 
-	
-	@property
-	def attack_list(self):
-		'''Get pokemon's attack_list'''
-		return self._attack_list
-	@attack_list.setter
-	def attack_list(self, value):
-		self.attack_list = value
+############# PROPERTIES ################################################################
 
-	def is_alive():
-		''' TODO: description
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        self._name = value
+    
+    @property
+    def level(self):
+        return self._level
+    @level.setter
+    def level(self, value):
+#### TESTAR PARA AS OUTRAS PROPERTIES ASSIM COMO ESSE TRY: ######   
+        try:
+            value = int(value)
+        except ValueError:
+            print ("ERROR: Tried to assign a not int value to pokemon's level")
+        else:
+            self._level = value 
+    
+    @property
+    def attack_list(self):
+        '''Get pokemon's attack_list'''
+        return self._attack_list
+    @attack_list.setter
+    def attack_list(self, value):
+        self._attack_list = value
 
-		:returns: TODO
-		'''
-		return self.health_power > 0
+    @property
+    def defense_force(self):
+        return stats.defense_force()
 
-	@property
-	def defense_force(self):
-		return stats.defense_force()
-	
-	def perform_attack(attack, onPokemon):
-		''' Calculates the damage of Pokemon's attack over the onPokemon
-		
-		:param attack: TODO
-		:param onPokemon: TODO
-		:returns: TODO
-		'''
-		
-		#compares all possibilities of primary and secondary types of Pokemon and onPokemon
-		compare_modifier = 	poke_type1.compare_to(poke_type1.onPokemon)*poke_type1.compare_to(poke_type2.onPokemon)*poke_type2.compare_to(poke_type1.onPokemon)*poke_type2.compare_to(poke_type2.onPokemon)
-							
-		modifier = compare_modifier*attack.stab(type())*stats.critical()*random.uniform(0.85,1)
-		damage = ((stats.attack_force()*attack.base()/onPokemon.defense_force())+2)*modifier
-		onPokemon.receive_damage(damage)
+########## METHODS ######################################################################
+    
+    def is_alive():
+        ''' TODO: description
 
-	def receive_damage(damage = 0):
-		''' TODO: description
+        :returns: TODO
+        '''
+        # TODO fix it!
+        return self.health_power > 0
 
-		:param damage: TODO
-		:returns: TODO
-		'''
-		try:
-			damage = int(damage)
-		except ValueError:
-			print ("ERROR: Tried to assign a not int value in damage")
-		else:
-			stats.decrease_life(damage)
+    def perform_move(move, onPokemon):
+        ''' Calculates the damage of Pokemon's attack over the onPokemon
+        
+        :param move: TODO
+        :param onPokemon: TODO
+        :returns: The damage received by the pokemon 
+        '''
+        compare_modifier = self.compare_types_to(onPokemon)
+        modifier = compare_modifier * move.stab(self._poke_type) * stats.critical() * random.uniform(0.85,1)
+        damage = (self._stats.attack_force() * move.power / onPokemon.defense_force() + 2) * modifier
+        onPokemon.receive_damage(damage)
+        return damage
+
+    def compare_types_to(pokemon):
+        ''' Compare every combination between two pokemon's types. 
+
+        :param pokemon: The deffensive pokemon.
+        :returns: The **Type** coefficient to be used on damage formula
+        '''
+        # TODO: Verify if it is realy a pokemon?
+        type_coef = 1
+        for type1 in self._type_list:
+            for type2 in pokemon.type_list:
+                type_coef *= type1.compare_to(type2)
+
+        return type_coef
+
+    def receive_damage(damage = 0):
+        ''' TODO: description
+
+        :param damage: TODO
+        :returns: TODO
+        '''
+        try:
+            damage = int(damage)
+        except ValueError:
+            print ("ERROR: Tried to assign a not int value in damage")
+        else:
+            stats.decrease_life(damage)
 
--- src/Stats.py
+++ src/Stats.py
@@ -4,19 +4,19 @@ class Stats:
     '''
     TODO: description
     '''
-    def __init__(self, hp, attack_power, defense_power, speed, special): # Se houvessem valores padroes deveriamos por? ou definir setters para se um dia houvessem 10 stats fosse mais facil de inicializar? x_x
+    def __init__(self, hp, attack, defense, speed, special): # Se houvessem valores padroes deveriamos por? ou definir setters para se um dia houvessem 10 stats fosse mais facil de inicializar? x_x
         '''TODO: description
 
         :param hp: TODO
-        :param attack_power: TODO
-        :param defense_power: TODO
+        :param attack: TODO
+        :param defense: TODO
         :param speed: TODO
         :param special: TODO
         :returns: TODO
         '''
         self._hp = hp
-        self._attack_power = attack_power
-        self._defense_power = defense_power
+        self._attack = attack
+        self._defense = defense
         self._speed = speed
         self._special = special
 
@@ -34,14 +34,14 @@ class Stats:
         :param level: TODO
         :returns: TODO
         '''
-        return (2 * level + 10) * self._attack_power
+        return (2 * level + 10) * self._attack
 
     def defense_force():
         '''TODO: description
 
         :returns: TODO
         '''
-        return 250 * self._defense_power
+        return 250 * self._defense
 
     def critical():
         '''TODO: description
