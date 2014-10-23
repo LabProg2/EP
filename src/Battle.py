bcb1495636faf67018d69baf406c4d492c2b2ec6
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2014-10-23 16:04:49 -0200

    few changes in battle.py

--- src/Battle.py
+++ src/Battle.py
@@ -1,28 +1,32 @@
 class Battle:
 	def __init__(self, poke1, poke2):
+		''' Make poke1 and poke2 battle	'''
 		self._poke1 = poke1
 		self._poke2 = poke2
 		self._turn = self.whos_faster(poke1, poke2)
 
 	def whos_faster(poke1, poke2):
-		return poke1 if poke1.get_spd() > poke2.get_spd() else return poke2
+		''' Decides which pokemon is faster '''
+		if poke1.get_spd() > poke2.get_spd(): 
+			return poke1
+		else:
+			return poke2
 
 	def run_battle():
+		''' Start the battle '''
 		pokeio = PokeIO()
-		active_poke = whos_faster(poke1, poke2)
-		while poke1.is_alive() or poke2.is_alive():
-			pokeio.print_poke_info(self.poke1, self.turn == 1)
-			pokeio.print_poke_info(self.poke2, self.turn == 2)
+		active_poke = whos_faster(self._poke1, self._poke2)
+		while self._poke1.is_alive() or self._poke2.is_alive():
+			pokeio.print_poke_info(self._poke1, active_poke is self._poke1)
+			pokeio.print_poke_info(self._poke2, active_poke is self._poke2)
 			pokeio.print_attack_list(active_poke)
 			attack = pokeio.read_attack
-			if active_poke is poke1:
-				next_poke = poke2
+			if active_poke is self._poke1:
+				next_poke = self._poke2
 			else:
-				next_poke = poke1
+				next_poke = self._poke1
 			damage = active_poke.perform_attack(attack, next_poke)
 			next_poke.receive_damage(damage)
 			active_poke = next_poke
-
-
 
 
