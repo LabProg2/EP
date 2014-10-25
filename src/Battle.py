78034578be425a16fe9d54bf1ef62a2c7114cd07
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2014-10-25 12:40:57 -0200

    Coisas que eu fiz pra juntar

--- src/Battle.py
+++ src/Battle.py
@@ -1,8 +1,22 @@
+from PokeIO import PokeIO
 class Battle:
     ''' The battle between two pokemons'''
-    def __init__(self, poke1, poke2):
+    def __init__(self, poke1_path, poke2_path):
         ''' Prepare battle between poke1 and poke2'''
         #verificar se poke1 e poke2 são pokemons e levantar exception
+        pokeio = PokeIO()
+        try:
+            poke1_f = open(poke1_path, 'r')
+        except FileNotFoundError:
+            print("The first pokemon file couldn't be read")
+        try:
+            poke2_f = open(poke1_path, 'r')
+        except FileNotFoundError:
+            print("The second pokemon file couldn't be read")
+
+        poke1 = pokeio.read_poke(poke1_f)
+        poke2 = pokeio.read_poke(poke2_f)
+
         if poke1.stats.speed > poke2.stats.speed:
             self._active_poke, self._idle_poke = poke1, poke2
         else:
@@ -10,7 +24,6 @@ class Battle:
 
     def run_battle(self):
         ''' Start the battle'''
-        pokeio = PokeIO()
         while self._active_poke.is_alive() or self._idle_poke.is_alive():
             pokeio.print_poke_info(self._active_poke, True)
             pokeio.print_poke_info(self._idle_poke, False)
--- src/PokeIO.py
+++ src/PokeIO.py
@@ -1,8 +1,72 @@
+from Stats import Stats
+from Move import Move
+from Type import Type
+from Pokemon import Pokemon
 class PokeIO:
     ''' 
     This class is responsible for every interaction between user and this pokemon game
     '''
-    def read_move(upper_limit, restriction_list = []):
+    def read_poke(self, poke_file):
+        name = poke_file.readline()
+        try:
+            lvl = poke_file.readline()
+        except ValueError:
+            print("Level must be an integer")
+        try:
+            hp = poke_file.readline()
+        except ValueError:
+            print("Health powe must be an integer")
+        try:
+            atk = poke_file.readline()
+        except ValueError:
+            print("Attack must be an integer")
+        try:
+            defe = int(poke_file.readline())
+        except ValueError:
+            print("Defence must be an integer")
+        try:
+            spd = int(poke_file.readline())
+        except ValueError:
+            print("Speed must be an integer")
+        try:
+            spc = int(poke_file.readline())
+        except ValueError:
+            print("Special must be an integer")
+        try:
+            tp = int(poke_file.readline())
+            typ1 = Type(tp)
+            tp = int(poke_file.readline())
+            typ2 = Type(tp)
+        except ValueError:
+            print("Pokemon Type must be an integer")
+        typ_list = typ1, typ2
+        nat = poke_file.readline()
+        stats = Stats(hp, atk, defe, spd, spc)
+        move_list = []
+        for i in range(int(nat)):
+            move_name = poke_file.readline()
+            try:
+                tp = int(poke_file.readline())
+                move_type = Type(tp)
+            except ValueError:
+                print("A move type must be an integer")
+            try:
+                move_acu = int(poke_file.readline())
+            except ValueError:
+                print("A move accuracy must be an integer")
+            try:
+                move_pwr = int(poke_file.readline())
+            except ValueError:
+                print("A move power must be an integer")
+            try:
+                move_pp = int(poke_file.readline())
+            except ValueError:
+                print("A move pp must be an integer")
+            move = Move(move_name, move_type, move_acu, move_pwr, move_pp)
+            move_list.append(move)
+        return Pokemon(typ_list, stats, name, lvl, move_list)
+
+    def read_move(self, upper_limit, restriction_list = []):
         ''' Reads a movement from the users keyboard
         :param upper_limit: The upper limit for the number entry
         :param restriction_list: Restrictions for the number entry
@@ -20,7 +84,7 @@ class PokeIO:
                 print("Por favor, seja bonzinho e digite um movimento válido.")
         return x #+ 1 ou não? hehe acho que sim
                 
-    def print_move_list(move_list):
+    def print_move_list(self, move_list):
         ''' Prints the list of movements a pokemon can make
         :param move_list: The list of movements thats going to be printed
         '''
@@ -30,7 +94,7 @@ class PokeIO:
             print("[" + i + "] - " + move.name + " (" + move.pp + ")")
             i = i + 1
 
-    def print_poke_info(pokemon, is_on_turn = False):
+    def print_poke_info(self, pokemon, is_on_turn = False):
         ''' Prints the info of a pokemon
         :param pokemon: The pokemon that will have its info printed
         :param is_on_turn: A flag to inform if the pokemon is on its is_on_turn
