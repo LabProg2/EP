62b7450ef587050183eac0e0a6ecc21e6788e451
Bruno <bruno.sesso@usp.br>
2014-10-25 12:21:40 -0200

    Corrigi ai

--- src/Pokemon.py
+++ src/Pokemon.py
@@ -36,10 +36,6 @@ class Pokemon:
     
         self.move_list = move_list
 
-        try:
-            type_list = list(type_list)
-        except ValueError:
-            raise ValueError("type_list must be a list")
 
         try:
             type_list[0] = int(type_list[0])
@@ -53,12 +49,11 @@ class Pokemon:
 
         if isinstance(type_list, Type):
             type_list = [type_list]
-        else:
-            raise TypeError("type_list must have only Type instances")
-
+        if not isinstance(type_list, list):
+            raise TypeError("type_list must be a list")
         type_list.append(Type.Blank)
         type_list = type_list[0 : 2]
-        if len(type_list) <= 1:
+        if len(type_list) == 1:
             raise ValueError("type_list must have at least one type")
         for typ in type_list:
             if not isinstance(typ, Type):
