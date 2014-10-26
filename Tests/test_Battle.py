358efbeea4649f512598ad1d37bf23a7521329f2
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2014-10-26 14:44:47 -0200

    opa.. tinha colocado uns tabs no meio, foi mal :D

---
+++ Tests/test_Battle.py
@@ -0,0 +1,11 @@
+import unittest
+import sys, os
+sys.path.insert(0, os.path.abspath('../src/'));
+from Battle import Battle
+
+class Test_Battle(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
